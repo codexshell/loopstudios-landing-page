d21d6e2ff29ab7c5f3c710b85ec4134a0b229d3a
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-31 20:52:01 +0000

    chore: add brand stytyles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screen: {
+        "2xl": "1440px",
+      },
+      colors: {
+        "brand-primary-white": "hsl(0, 0%, 100%)",
+        "brand-primary-black": "hsl(0, 0%, 0%)",
+        "brand-primary-gray-1": "hsl(0, 0%, 55%)",
+        "brand-primary-gray-2": "hsl(0, 0%, 41%)",
+      },
+      fontSize: {
+        base: "15px",
+      },
+      fontFamily: {
+        Alata: ["Alata", "sans-serif"],
+        "Josefin": ["Josefin Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
