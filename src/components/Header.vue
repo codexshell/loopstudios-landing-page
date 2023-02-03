2a0a5d1c9737226bc5d0429b5910855247f93e66
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-02-03 19:15:26 +0000

    fix: add hover states to global header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,17 +8,20 @@
       <button class="xl:hidden" v-on:click="handleClick">
         <img v-bind:src="hamburgerUrl" alt="main menu" />
       </button>
-      <nav>
+      <nav class="hidden xl:block">
         <ul
           class="hidden xl:flex xl:gap-6 xl:text-brand-primary-white xl:font-brand-1"
         >
-          <li v-for="navigation in navigationList">
+          <li v-for="navigation in navigationList" v-bind:key="navigation">
             <a v-on:click.prevent href="/">{{ navigation }}</a>
+            <hr
+              class="border-[.1rem] hover:border-[0.1rem] w-6 mx-auto mt-[0.25rem]"
+            />
           </li>
         </ul>
       </nav>
     </div>
-    <div class="border-2 mt-48 xl:inline-block xl:w-3/5">
+    <div class="border-2 mt-48 xl:inline-block xl:w-3/5 xl:mt-32">
       <h1
         class="text-brand-primary-white font-brand-2 text-5xl p-4 sm:text-center xl:text-7xl xl:text-start xl:p-8"
       >
@@ -40,6 +43,7 @@
           <li
             class="text-brand-primary-white uppercase font-brand-2 text-3xl [&:not(:first-child)]:mt-8"
             v-for="navigation in navigationList"
+            v-bind:key="navigation"
           >
             <a class="sm:px-12" href="/">
               {{ navigation }}
@@ -62,3 +66,13 @@ let drawerOpen = $ref(false);
 
 const handleClick = () => (drawerOpen = !drawerOpen);
 </script>
+
+<style scoped>
+hr {
+  visibility: hidden;
+}
+
+a:active + hr {
+  visibility: visible;
+}
+</style>
