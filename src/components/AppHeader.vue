c07cd37c59c24e58ee4895c65f3f590a9cc284e9
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-02-09 00:33:37 +0300

    chore: add unit test for AppHeader component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,58 +1,64 @@
 <template>
-  <div
-    v-if="!drawerOpen"
-    class="bg-mobile-hero-pattern bg-no-repeat px-8 pt-10 pb-52 md:bg-desktop-hero-pattern xl:px-40 xl:pt-16"
-  >
-    <div class="flex items-center justify-between">
-      <div><img v-bind:src="logoUrl" alt="logo" /></div>
-      <button class="xl:hidden" v-on:click="handleClick">
-        <img v-bind:src="hamburgerUrl" alt="main menu" />
-      </button>
-      <nav class="hidden xl:block">
-        <ul
-          class="hidden xl:flex xl:gap-6 xl:font-brand-1 xl:text-brand-primary-white"
+  <header>
+    <div
+      v-if="!isMenuOpen"
+      class="bg-mobile-hero-pattern bg-no-repeat px-8 pt-10 pb-52 md:bg-desktop-hero-pattern xl:px-40 xl:pt-16"
+    >
+      <div class="flex items-center justify-between">
+        <div><img v-bind:src="logoUrl" alt="logo" /></div>
+        <button
+          data-test="menu-button"
+          class="xl:hidden"
+          v-on:click="handleClick"
         >
-          <li v-for="navigation in navigationList" v-bind:key="navigation">
-            <a v-on:click.prevent href="/">{{ navigation }}</a>
-            <hr
-              class="mx-auto mt-[0.25rem] w-6 border-[.1rem] hover:border-[0.1rem]"
-            />
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div class="mt-48 border-2 xl:mt-32 xl:inline-block xl:w-3/5">
-      <h1
-        class="p-4 font-brand-2 text-[2.5rem] text-brand-primary-white sm:text-center xl:p-8 xl:text-start xl:text-7xl"
-      >
-        IMMERSIVE EXPERIENCES THAT DELIVER
-      </h1>
-    </div>
-  </div>
-
-  <div v-if="drawerOpen" class="bg-brand-primary-black px-8 pt-10 pb-52">
-    <div class="flex items-center justify-between">
-      <div><img v-bind:src="logoUrl" alt="logo" /></div>
-      <button v-on:click="handleClick">
-        <img v-bind:src="iconCloseUrl" alt="main menu" />
-      </button>
-    </div>
-    <div class="mt-48">
-      <nav>
-        <ul class="sm:text-center">
-          <li
-            class="font-brand-2 text-3xl uppercase text-brand-primary-white [&:not(:first-child)]:mt-8"
-            v-for="navigation in navigationList"
-            v-bind:key="navigation"
+          <img v-bind:src="hamburgerUrl" alt="main menu" />
+        </button>
+        <nav class="hidden xl:block" data-test="desktop-menu">
+          <ul
+            class="hidden xl:flex xl:gap-6 xl:font-brand-1 xl:text-brand-primary-white"
           >
-            <a class="sm:px-12" href="/">
-              {{ navigation }}
-            </a>
-          </li>
-        </ul>
-      </nav>
+            <li v-for="navigation in navigationList" v-bind:key="navigation">
+              <a v-on:click.prevent href="/">{{ navigation }}</a>
+              <hr
+                class="mx-auto mt-[0.25rem] w-6 border-[.1rem] hover:border-[0.1rem]"
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="mt-48 border-2 xl:mt-32 xl:inline-block xl:w-3/5">
+        <h1
+          class="p-4 font-brand-2 text-[2.5rem] text-brand-primary-white sm:text-center xl:p-8 xl:text-start xl:text-7xl"
+        >
+          IMMERSIVE EXPERIENCES THAT DELIVER
+        </h1>
+      </div>
     </div>
-  </div>
+
+    <div v-if="isMenuOpen" class="bg-brand-primary-black px-8 pt-10 pb-52">
+      <div class="flex items-center justify-between">
+        <div><img v-bind:src="logoUrl" alt="logo" /></div>
+        <button v-on:click="handleClick" data-test="menu-button">
+          <img v-bind:src="iconCloseUrl" alt="main menu" />
+        </button>
+      </div>
+      <div class="mt-48">
+        <nav data-test="mobile-menu">
+          <ul class="sm:text-center">
+            <li
+              class="font-brand-2 text-3xl uppercase text-brand-primary-white [&:not(:first-child)]:mt-8"
+              v-for="navigation in navigationList"
+              v-bind:key="navigation"
+            >
+              <a class="sm:px-12" href="/">
+                {{ navigation }}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts">
@@ -63,12 +69,12 @@ import { navigationList } from "../data/header";
 import { reactive, toRef } from "vue";
 
 const state = reactive({
-  drawerOpen: false,
+  isMenuOpen: false,
 });
 
-const handleClick = () => (state.drawerOpen = !state.drawerOpen);
+const handleClick = () => (state.isMenuOpen = !state.isMenuOpen);
 
-const drawerOpen = toRef(state, "drawerOpen");
+const isMenuOpen = toRef(state, "isMenuOpen");
 </script>
 
 <style scoped>
