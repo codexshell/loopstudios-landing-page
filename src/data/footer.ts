afe0c250b76429b131c3e69039e152451864d147
Silvanos Eric <ericoderito@gmail.com>
2023-02-11 23:00:36 +0000

    chore: add footer section

---
+++ src/data/footer.ts
@@ -0,0 +1,18 @@
+import facebookIconUrl from "../assets/icon-facebook.svg";
+import twitterIconUrl from "../assets/icon-twitter.svg";
+import pinterestIconUrl from "../assets/icon-pinterest.svg";
+import instaIconUrl from "../assets/icon-instagram.svg";
+
+export const footerLinksList = [
+  "About",
+  "Careers",
+  "Events",
+  "Products",
+  "Support",
+];
+export const socialLinksList = [
+  { url: facebookIconUrl, alt: "facebook" },
+  { url: twitterIconUrl, alt: "twitter" },
+  { url: pinterestIconUrl, alt: "pinterest" },
+  { url: instaIconUrl, alt: "instagram" },
+];
